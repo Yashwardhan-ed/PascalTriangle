148da816bfac1f80100dc5c8731bc96be357afae
Yashwardhan-ed <2006.yws@gmail.com>
2024-10-11 12:52:49 +0530

    Add files via upload

---
+++ bubble.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Bubble {
+    static void sort(int array[]){
+	int min=array[0];
+	for(int i=0;i<arr.length;i++){
+	    for(int j=i;j<arr.length;j++){
+		int 
+		if(array[i]>array[j]){
+		    int temp=array[i];
+		    array[i]=array[j];
+		    array[j]=temp;
+		}
+	    }
+	}
+    }
+    static void swapmin(int array[],int s){
+	int minIndex=s;
+	for(int i=s+1;i<array.length;i++){
+	    if(array[i]<array[minIndex]{
+		minIndex=i;
+		}
+	}
+    }
+
+    /*
+    static void selectionSort(int[] array){
+    for(int i=0;i<array.length;i++){
+	swapmin(array,i);
+        }
+    }
+	
+}
